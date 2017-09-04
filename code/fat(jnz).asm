04503cb233e95726a536687e2e8ba0745102b742
Raffael Bottoli Schemmer <raffael.schemmer@acad.pucrs.br>
2017-09-03 22:06:57 -0300

    Create fat(jnz).asm

---
+++ code/fat(jnz).asm
@@ -0,0 +1,22 @@
+.text
+
+# Inicializa $r0 com o byte [0] a ser calculado (fatorial)
+li 0,0
+lb $r0,$t0
+
+
+# Inicializa $r1 com 1 a ser o buffer do fatorial
+li 0,1
+mov $t0,$r1
+
+mul 00,1,$r0,$r1
+dec 00,0,$r0
+jnz -3
+
+li 0,1
+lw $r1,$t0
+
+.data
+
+.byte 5
+.word 0
