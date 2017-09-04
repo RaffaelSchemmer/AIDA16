86cd9aa4610853b0ce64d3e9f3f199f56a1ef6e8
Raffael Bottoli Schemmer <raffael.schemmer@acad.pucrs.br>
2017-09-03 22:31:56 -0300

    Update fat(jnz).asm

--- code/fat(jnz).asm
+++ code/fat(jnz).asm
@@ -4,24 +4,17 @@
 li 0,0
 lb $r0,$t0
 
-# Inicializa $r1 com N com o valor decrementado do fatorial
-mov $r0,$r1
 
-# Inicializa $t0 com 1 a ser o contador do programa
+# Inicializa $r1 com 1 a ser o buffer do fatorial
 li 0,1
+mov $t0,$r1
 
-# Inicializa $r2 com N a ser o contador do fatorial
-mov $r1,$r2
-
-dec 00,0,$r1
-mul 00,0,$r2,$r1
-inc 00,0,$t0
-mov $r0,$r3
-div 00,1,$t0,$r3
-jz -6
+mul 00,1,$r0,$r1
+dec 00,0,$r0
+jnz -3
 
 li 0,1
-sw $t0,$r2
+sw $t0,$r1
 
 halt
 
@@ -29,4 +22,3 @@ halt
 
 .byte 5
 .word 0
-
